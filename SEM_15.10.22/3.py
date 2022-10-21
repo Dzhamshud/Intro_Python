ebc23689fd4bd15ccf4600c6c630fbfea6594684
Pavel.Koshelev <koshelev_p@mail.ru>
2022-10-21 23:10:27 +0300

    Домашнее задание к третьему семинару

---
+++ SEM_15.10.22/3.py
@@ -0,0 +1,19 @@
+letter = 3
+
+def input_data():  # объявление подпрограммы(функции)
+    pass
+
+
+def change_data(a, b, c, name='Evgeniy'):
+    answer = a+b+c
+    print(name)
+    return answer
+
+
+def print_data():
+    pass
+
+
+input_data()  # вывзов продпрограммы(функции)
+print(change_data(1, 2, letter, name='Pavel'))
+print_data()
